4d08eb06909e735a48df5afd15aed85631d03a74
Saravanan Prasadh <saravanan.prasadh@starburstdata.com>
2024-08-08 19:04:03 +0530

    Adding the entire library and forked version cause the Pom is getting complicated than necessary

---
+++ secrets-keystore-plugin/src/main/java/io/airlift/secrets/symDecrypt/symDecryptSecretProvider.java
@@ -0,0 +1,90 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.airlift.secrets.symDecrypt;
+
+import com.google.inject.Inject;
+import io.airlift.spi.secrets.SecretProvider;
+
+// symmetric decryption imports
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.spec.KeySpec;
+import java.util.Base64;
+
+// General imports
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+public class symDecryptSecretProvider
+        implements SecretProvider
+{
+    private static final byte[] encryptPassword = {83, 116, 97, 114, 98, 117, 114, 115, 116, 82, 48, 99, 107, 115, 33};
+
+    @Inject
+    public symDecryptSecretProvider(symDecryptSecretProviderConfig config)
+            throws GeneralSecurityException, IOException
+    {
+        algorithm = config.getAlgorithm();
+        secretKeyAlgorithm = config.getSecretKeyAlgorithm();
+        keyLength = config.getKeyLength();
+        iterationCount = config.getIterationCount();
+    }
+
+    @Override
+    public String resolveSecretValue(String encryptedValue)
+    {
+    // Using the OpenSSL to symmetrically decrypt the encrypted value
+    // command used to create the encrypted
+    // echo -n "Your secret message" | openssl enc -aes-256-cbc -base64 -pass pass:"your_password" -pbkdf2
+    // pin the decrytion Password.
+    return decrypt(encryptedValue);
+    }
+    public static String decrypt(String cipherText) throws Exception {
+
+        byte[] cipherData = Base64.getDecoder().decode(cipherText);
+        byte[] saltData = extractSalt(cipherData);
+        byte[] encData = extractEncryptedData(cipherData);
+
+        SecretKey key = generateKey(encryptPassword, saltData);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(saltData));
+        byte[] decryptedData = cipher.doFinal(encData);
+        // property 
+        System.setProperty(net.ssh, cipherText)
+        return new String(decryptedData, StandardCharsets.UTF_8);
+    }
+
+    private static SecretKey generateKey(byte[] password, byte[] salt) throws Exception {
+        SecretKeyFactory factory = SecretKeyFactory.getInstance(secretKeyAlgorithm);
+        KeySpec spec = new PBEKeySpec(password.toString(), salt, iterationCount, keyLength);
+        return new SecretKeySpec(factory.generateSecret(spec).getEncoded(), "AES");
+    }
+
+    private static byte[] extractSalt(byte[] cipherData) {
+        byte[] saltData = new byte[8];
+        System.arraycopy(cipherData, 8, saltData, 0, 8);
+        return saltData;
+    }
+
+    private static byte[] extractEncryptedData(byte[] cipherData) {
+        byte[] encData = new byte[cipherData.length - 16];
+        System.arraycopy(cipherData, 16, encData, 0, encData.length);
+        return encData;
+    }
+}
